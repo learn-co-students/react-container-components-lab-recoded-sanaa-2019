ebf9a3005ce2b78e03f3d0dce2d7b69e10163484
Taghreed Nageeb <taghreed.n.d2018@gmail.com>
2019-12-09 17:25:07 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+ 
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super()
+ 
+    this.state = {
+        reviews:[]
+    };
+  }
+ 
+  componentDidMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => this.setState({ reviews: data.result }))
+  }
+ 
+  render() {
+    return (
+    <div className="latest-movie-reviews">
+    <MovieReviews reviews={this.state.reviews}/>
+    </div>)
+  }
+}
+ 
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+const MovieReviews=({reviews})=>(
+    <div className="review-list">{reviews.map((review,index)=>
+        {return(
+        <div key={index} className="review">
+        <h1 >{review.display_title}</h1>
+        <img src={review.multimedia.src} alt=""/>
+        <p>{review.summary_short}</p>
+        </div>
+    )})}
+    </div>
+)
+export default MovieReviews 
+MovieReviews.defaultProps = {
+    reviews: []
+  };
