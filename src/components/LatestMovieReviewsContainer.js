d2d2d60a95bbab84f3d1bb85cdf8f6460990d047
ahmed-hassany <ahmedalhassany1@gmail.com>
2019-12-15 06:19:59 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+//const NYT_API_KEY = 'ahmedmovieapikey';
+export default class LatestMovieReviewsContainer extends Component{
+    constructor(){
+        super();
+        this.state = {
+            reviews:[]
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL).then(res=>res.json()).then(data=>{
+            console.log(data);
+            this.setState({
+                reviews:data.results,
+            })
+        }).catch(err=>console.log(err));
+    }
+
+    render(){
+        return(
+            <div className="latest-movie-reviews" style={{margin:'40px auto'}}>
+            <MovieReviews reviews={this.state.reviews} />
+            </div>
+        );
+    }
+}
