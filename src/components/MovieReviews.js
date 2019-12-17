f230a5017602523587e038b0af779e262c3317df
Sarah Alafif <sar1997yah@gmail.com>
2019-12-17 20:02:17 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
-// Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = props => {
+  return (
+    <div className="review-list col raw ">
+      {props.reviews.map(movie => (
+        <div className="card review float-left m-2" style={{ width: "18rem" }}>
+          <img
+            src={movie.multimedia ? movie.multimedia.src : ""}
+            className="card-img-top"
+            alt=""
+          />
+          <div className="card-body">
+            <h5 className="card-title">{movie.display_title}</h5>
+            <p className="card-text">{movie.summary_short}</p>
+            <a href={movie.link.url} className="btn btn-primary">
+              Go Review
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MovieReviews;
